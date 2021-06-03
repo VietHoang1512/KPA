238913c65be6e27ec1844c28af9d9cb62f24d7d3
leonard <leonard@cinnamon.is>
2021-06-03 21:04:37 +0700

    Add running scripts

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-# linux
-export PYTHONPATH=$PWD
-python src/scripts/run_baselines.py --output_dir "tmp" --model_name "roberta-base" --do_train --overwrite_output_dir --max_len 6 --argument_max_len 12 --train_batch_size 16 --val_batch_size 4
-
-
- 
