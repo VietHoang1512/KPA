d0009eeb0338d02f975ff671855d8fef2f794421
leonard <leonard@cinnamon.is>
2021-06-07 12:07:11 +0700

    Fix parallelism tokenizer

--- bin/run_baselines.sh
+++ bin/run_baselines.sh
@@ -4,6 +4,7 @@ python src/scripts/run_baselines.py \
         --output_dir "outputs" \
         --model_name "roberta-base" \
         --directory "kpm_data" \
+        --logging_dir "runs" \
         --overwrite_output_dir \
         --num_train_epochs 15 \
         --early_stop 5 \
@@ -11,14 +12,14 @@ python src/scripts/run_baselines.py \
         --val_batch_size 64 \
         --do_train \
         --evaluate_during_training \
-        --warmup_steps 250 \
-        --gradient_accumulation_steps 2 \
+        --warmup_steps 0 \
+        --gradient_accumulation_steps 1 \
         --learning_rate 0.00003 \
         --margin 1.0 \
-        --drop_rate 0.15 \
-        --n_hiddens 2 \
-        --max_len 36 \
-        --argument_max_len 64 \
+        --drop_rate 0.1 \
+        --n_hiddens 4 \
+        --max_len 24 \
+        --argument_max_len 48 \
         --stance_dim 32 \
         --text_dim 256 \
         --num_workers 2 \
