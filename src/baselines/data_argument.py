88042e05438f9a137aba44137698471400f387a4
VietHoang1710 <phanviethoang1512@gmail.com>
2021-06-18 10:49:40 +0000

    Add inheritance

--- src/baselines/data_argument.py
+++ src/baselines/data_argument.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass, field
 from typing import Optional
 
+from src.train_utils.base_arguments import BaseDataArguments
+
 
 @dataclass
-class DataArguments:
+class DataArguments(BaseDataArguments):
 
     """Arguments pertaining to what data we are going to input our model for training and eval."""
 
-    directory: Optional[str] = field(default=None, metadata={"help": "The input data folder"})
     max_len: Optional[int] = field(default=36, metadata={"help": "max sequence length for topics and keypoints"})
     argument_max_len: Optional[int] = field(default=48, metadata={"help": "max sequence length for arguments"})
