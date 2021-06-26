29314076c55055a1b85f0bf5bc90c4db88e1d1cf
VH Azure <phanviethoang1512@gmail.com>
2021-06-26 03:46:58 +0000

    Add private test pretraining

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,3 +3,5 @@
 pip install -r requirements.txt
 
 [[ -d kpm_6_folds ]] || ((gdown --id 1UawLUw-3dKYEpYaRhmo8l6eb8xlSksLB) && (unzip "kpm_6_folds.zip") && rm "kpm_6_folds.zip")
+
+[[ -d IBM_Debater ]] || ((gdown --id 1ZbXhnhGOT8vmPJ6mVqI_fX4q5ztoVGR_) && (unzip "IBM_Debater.zip") && rm "IBM_Debater.zip")
