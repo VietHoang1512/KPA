62ca79c5bff8e83596b000d5864c15917008a434
VH Azure <phanviethoang1512@gmail.com>
2021-06-27 13:35:27 +0000

    Add bash scripts

--- bin/train_qa.sh
+++ bin/train_qa.sh
@@ -1,32 +1,34 @@
+%%writefile bin/train_qa.sh
 export PYTHONPATH=$PWD
-for fold_id in 1 2 3 4 5 6
+for fold_id in 1 2 3 4 
 do
         python src/scripts/train_qa.py \
                 --output_dir "outputs/qa/fold_$fold_id" \
-                --model_name_or_path "distilbert-base-uncased" \
-                --tokenizer "distilbert-base-uncased" \
+                --model_name_or_path "roberta-base" \
+                --tokenizer "roberta-base" \
                 --loss_fct "online-constrastive" \
                 --distance "cosine" \
-                --directory "kpm_6_folds/fold_$fold_id" \
-                --test_directory "kpm_6_folds/test/" \
+                --directory "kpm_k_folds/fold_$fold_id" \
+                --test_directory "kpm_k_folds/test/" \
                 --logging_dir "runs/qa/fold_$fold_id" \
+                --logging_steps 40 \
                 --overwrite_output_dir \
                 --num_train_epochs 15 \
                 --early_stop 5 \
-                --train_batch_size 8 \
-                --val_batch_size 8 \
+                --train_batch_size 128 \
+                --val_batch_size 128 \
                 --do_train \
                 --evaluate_during_training \
                 --warmup_steps 0 \
                 --gradient_accumulation_steps 1 \
                 --learning_rate 0.00003 \
-                --margin 0.5 \
+                --margin 0.3 \
                 --drop_rate 0.1 \
-                --n_hiddens -1 \
-                --max_topic_length 16 \
-                --max_statement_length 48 \
+                --n_hiddens 4 \
+                --max_topic_length 10 \
+                --max_statement_length 45 \
                 --stance_dim 32 \
                 --text_dim 256 \
-                --num_workers 8 \
-                --seed 0
+                --num_workers 4 \
+                --seed 0 
 done
