03d8d9ca8b2c752018f61f05d1d82e6d3ef1f436
VietHoang1512 <phanviethoang1512@gmail.com>
2021-07-01 10:24:35 +0700

    Python package release

--- bin/train_qa.sh
+++ bin/train_qa.sh
@@ -1,7 +1,7 @@
 export PYTHONPATH=$PWD
 for fold_id in 1 2 3 4 
 do
-        python src/scripts/main.py \
+        python scripts/main.py \
                 --experiment "qa" \
                 --output_dir "outputs/qa/fold_$fold_id" \
                 --model_name_or_path "roberta-base" \
